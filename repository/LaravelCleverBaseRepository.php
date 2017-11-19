098b132bb00e80c7db64e997679bf5a991a7f45e
Artu1000 <arthur.lorent@gmail.com>
2017-11-19 13:39:28 +0100

    updated package structure

--- repository/LaravelCleverBaseRepository.php
+++ repository/LaravelCleverBaseRepository.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Okipa\LaravelCleverBaseRepository;
+namespace Okipa\Repository\LaravelCleverBaseRepository;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
