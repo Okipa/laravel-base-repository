614aef341f1e2391d810d119e246b62f26a42d38
Artu1000 <arthur.lorent@gmail.com>
2017-11-19 19:27:43 +0100

    added service provider and updated readme

--- src/Traits/ConfigChecksTrait.php
+++ src/Traits/ConfigChecksTrait.php
@@ -67,7 +67,7 @@ trait ConfigChecksTrait
             );
         }
         // we check if the config json storage instruction is a boolean value
-        if (!is_bool($this->configKey . '.json_storage')) {
+        if (!is_bool(config($this->configKey . '.json_storage'))) {
             throw new ErrorException(
                 get_class($this) . ' : the config "' . $this->configKey 
                 . '.json_storage" instruction is not a boolean value.'
